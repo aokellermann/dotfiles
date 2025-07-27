75d60550c8c1844ccf7218aaece9646bd93c2cd4
Antony Kellermann <antony@aokellermann.dev>
2025-07-27 16:58:49 -0700

    auto monitor config

---
+++ .local/bin/sway-wins.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ -z "$@" ]; then
+    displays="$(swaymsg -t get_outputs | jq -r '.[] | select(.active == true) | .make + " " + .model + " " + .serial')"
+else
+    displays=$@
+fi
+
+config_file="$HOME/.config/sway_win_extra"
+rm -f "$config_file"
+
+wallpaper_file="$HOME/images/wallpapers/cityscape_synthwave_moon_buildings_4k_hd_vaporwave.jpg"
+
+i=1
+echo "$displays" | while IFS= read -r d; do
+    swaymsg workspace "$i", move workspace to output \'"$d"\', output \'"$d"\' bg "$wallpaper_file" fill
+    echo "$d" >> "$config_file"
+    i=$((i+1))
+done
+
+printf "%s" "$(< "$config_file")"
